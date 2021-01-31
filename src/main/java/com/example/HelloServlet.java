d5c49ef3ed6089f1cf32180b1690a8ededd0dcdb
sehuisong <songsh428@gmail.com>
2021-01-31 22:52:50 +0900

    스프링 IoC 컨테이너 연동

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
 public class HelloServlet extends HttpServlet {
 	
 	/**
@@ -26,11 +29,16 @@ public class HelloServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 	        throws ServletException, IOException {
 		System.out.println("doGet");
+		
+		ApplicationContext context = (ApplicationContext) getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+		HelloService helloService = context.getBean(HelloService.class);
+		
 		resp.getWriter().println("<html>");
 		resp.getWriter().println("<head>");
 		resp.getWriter().println("</head>");
 		resp.getWriter().println("<body>");
-		resp.getWriter().println("<h1>Hello, " + getServletContext().getAttribute("name") + "</h1>");
+//		resp.getWriter().println("<h1>Hello, " + getServletContext().getAttribute("name") + "</h1>");
+		resp.getWriter().println("<h1>Hello, " + helloService.getName() + "</h1>");
 		resp.getWriter().println("</body>");
 		resp.getWriter().println("</html>");
 	}
